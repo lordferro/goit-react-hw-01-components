0a7dc2763fdc934bdd8a38d8365784da5fac00fb
Fedor Romanovschi <110813692+lordferro@users.noreply.github.com>
2023-03-19 17:03:08 +0100

    1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,20 +1,20 @@
 import user from 'user.json';
 import data from 'data.json';
-import friends from 'friends.json';
-// import transactions from 'transactions.json';
+// import friends from 'friends.json';
+import transactions from 'transactions.json';
 
 import { Profile } from 'components/Profile/Profile';
 import { Statistics } from 'components/Statistics/Statistics';
-import { FriendList } from 'components/FriendList/FriendList';
-// import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
+// import { FriendList } from 'components/FriendList/FriendList';
+import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
 
 export const App = () => {
   return (
     <div>
       <Profile user={user} />
       <Statistics title="Upload stats" data={data} />
-      <FriendList friends={friends} />
-      {/* <TransactionHistory transactions={ transactions} /> */}
+      {/* <FriendList friends={friends} /> */}
+      <TransactionHistory transactions={ transactions} />
     </div>
   );
 };
